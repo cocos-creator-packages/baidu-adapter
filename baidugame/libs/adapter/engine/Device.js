985ed25bb406a97bba532a1fc87faaeafd64d387
PP <chinese1236@qq.com>
2019-01-25 16:26:21 +0800

    adapt touchEvent in open data context

--- baidugame/libs/adapter/engine/Device.js
+++ baidugame/libs/adapter/engine/Device.js
@@ -71,23 +71,45 @@ function initSystemInfo () {
 window.__device = {
     init (cb) {
         initSystemInfo();
-        // send systemInfo to subDomain
+        // send main context info to subDomain
         if (swan.getOpenDataContext) {
             swan.getOpenDataContext().postMessage({
                 fromAdapter: true,
-                event: 'systemInfo',
-                info: JSON.stringify(systemInfo),
+                event: 'main-context-info',
+                sysInfo: JSON.stringify(systemInfo),
+                innerWidth: window.innerWidth,
+                innerHeight: window.innerHeight,
+                devicePixelRatio: window.devicePixelRatio,
             });
             cb && cb();
         }
         else {
             swan.onMessage(function (data) {
                 if (data.fromAdapter) {
-                    if (data.event === 'systemInfo') {
-                        let info = JSON.parse(data.info);
+                    if (data.event === 'main-context-info') {
+                        let info = JSON.parse(data.sysInfo);
                         // info addon systemInfo
                         Object.assign(info, systemInfo);
                         Object.assign(systemInfo, info);
+
+                        Object.defineProperty(window, 'innerWidth', {
+                            enumerable: true,
+                            get () {
+                                return data.innerWidth;
+                            },
+                        });
+                        Object.defineProperty(window, 'innerHeight', {
+                            enumerable: true,
+                            get () {
+                                return data.innerHeight;
+                            },
+                        });
+                        Object.defineProperty(window, 'devicePixelRatio', {
+                            enumerable: true,
+                            get () {
+                                return data.devicePixelRatio;
+                            },
+                        });
                         
                         cb && cb();
                     }
